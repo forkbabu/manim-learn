68d36398ce7ab4c3d6a9eba86091a51fbf52f8c7
ucalyptus <santaclaus30011998@gmail.com>
2021-06-23 14:45:42 +0530

    first commit

---
+++ MFB.py
@@ -0,0 +1,20 @@
+from manim import *
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text=MathTex(
+            "\\frac{d}{dx}f(x)g(x)=","f(x)\\frac{d}{dx}g(x)","+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        text = MathTex("dx ="," [f(x,t) - g^2(t)\\nabla_x \log p_t(x)]dt" ,"+"," g(t)d\\bar{\\textbf{w}}")
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()
